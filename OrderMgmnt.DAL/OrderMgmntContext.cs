6a1ce5e8df61d414b02eca1b26a7dca765650ef3
Maxim <maxim.khachatryan.2013@gmail.com>
2023-03-23 22:58:20 +0400

    Add DbContext

---
+++ OrderMgmnt.DAL/OrderMgmntContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderMgmnt.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderMgmnt.DAL
+{
+    public class OrderMgmntContext : DbContext
+    {
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Vender> Venders { get; set; }
+
+
+    }
+}
